27372eb58c02ea86c7a18092077b0446f4eeb0cd
Gabriel <gabrielflororosa2007@gmail.com>
2026-01-21 17:29:09 -0300

    fix var type

--- ERP-Backend/DTOs/Customer/CustomerCreateDto.cs
+++ ERP-Backend/DTOs/Customer/CustomerCreateDto.cs
@@ -2,6 +2,6 @@ public class CustomerCreateDto
 {
     public string Name { get; set; }
     public string Cpf { get; set; }
-    public string PhoneNumber { get; set; }
+    public int PhoneNumber { get; set; }
     public string Email { get; set; }
 }
