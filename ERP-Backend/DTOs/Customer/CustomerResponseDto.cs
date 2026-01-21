ddaf2df20615082500d8924ba5e0507835779f8d
Gabriel <gabrielflororosa2007@gmail.com>
2026-01-21 17:31:54 -0300

    aaaa

--- ERP-Backend/DTOs/Customer/CustomerResponseDto.cs
+++ ERP-Backend/DTOs/Customer/CustomerResponseDto.cs
@@ -3,7 +3,7 @@ public class CustomerResponseDto
     public int Id { get; set; }
     public string Name { get; set; }
     public string Cpf { get; set; }
-    public string PhoneNumber { get; set; }
+    public int PhoneNumber { get; set; }
     public string Email { get; set; }
     public bool Active { get; set; }
     public DateTime CreatedAt { get; set; }
--- ERP-Backend/DTOs/Customer/CustomerUpdateDto.cs
+++ ERP-Backend/DTOs/Customer/CustomerUpdateDto.cs
@@ -2,7 +2,7 @@ public class CustomerUpdateDto
 {
     public string Name { get; set; }
     public string Cpf { get; set; }
-    public string PhoneNumber { get; set; }
+    public int PhoneNumber { get; set; }
     public string Email { get; set; }
     public bool Active { get; set; }
 }
