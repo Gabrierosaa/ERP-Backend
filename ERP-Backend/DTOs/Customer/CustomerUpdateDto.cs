b993dd2f6831ce67c72897c65d628f183234ad87
Gabriel <gabrielflororosa2007@gmail.com>
2026-01-21 17:21:30 -0300

    Configuração dos DTOs das entidades do projeto

---
+++ ERP-Backend/DTOs/Customer/CustomerUpdateDto.cs
@@ -0,0 +1,8 @@
+public class CustomerUpdateDto
+{
+    public string Name { get; set; }
+    public string Cpf { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public bool Active { get; set; }
+}
